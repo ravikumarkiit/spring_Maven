f9476fdbf7c12457e1d00029305e5b6ed6d02510
ravikumarkiit <ravikumar.kiit@gmail.com>
2018-07-24 20:21:58 -0500

    Initial commit

---
+++ src/main/java/com/SpringMavenApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenApplication.class, args);
+	}
+}
